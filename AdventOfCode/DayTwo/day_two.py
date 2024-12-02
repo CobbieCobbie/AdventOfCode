67b3b61b907da1704f62b45effe7d6b4c421bab1
Benjamin Coban <coban@fzi.de>
2024-12-02 13:23:28 +0100

    Day Two, 3/50

--- AdventOfCode/DayTwo/day_two.py
+++ AdventOfCode/DayTwo/day_two.py
@@ -1,12 +1,15 @@
 def main():
     
     safeness_counter = 0
-    
+    line_counter = 0
     input = read_input()
     for line in input:
+        line_counter += 1
         numbers = line.split()
-        if safeness_check(numbers):
+        numbers_int = [int(i) for i in numbers]
+        if safeness_check(numbers_int):
             safeness_counter += 1
+    print(f"Number of lines: {line_counter}")
     print(f"Number of safe lines: {safeness_counter}")
 
 
@@ -24,6 +27,20 @@ def safeness_check(list):
         return True
     for i in range(len(list)-1):
         if abs(list[i] - list[i+1]) < delta_min or abs(list[i] - list[i+1]) > delta_max:
+            return False
+    return (isIncreasing(list) or isDecreasing(list))
+
+
+def isIncreasing(list):
+    for i in range(len(list)-1):
+        if list[i] >= list[i+1]:
+            return False
+    return True
+
+
+def isDecreasing(list):
+    for i in range(len(list)-1):
+        if list[i] <= list[i+1]:
             return False
     return True
 
