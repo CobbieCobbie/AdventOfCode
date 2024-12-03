648dbb93bfab19b7640bb53aa8158a80d5afa15a
Benjamin Coban <Benjamin.Coban@gmail.com>
2024-12-03 21:46:42 +0100

    Day Three, 5/50

--- AdventOfCode/DayThree/day_three.py
+++ AdventOfCode/DayThree/day_three.py
@@ -1,11 +1,52 @@
+from functools import reduce
+from operator import mul
+
 import re
 
 
 def main():
-    pattern = r'mul\(\d{1,3},\d{1,3}\)'
-    if re.fullmatch(pattern, "mul(1,3)"):
-        print("Matched")
+    print("First result", calc_part_one(read_input()))
+    print("Second result", calc_part_two(read_input()))
+
+def calc_part_one(input):
+    pattern = get_regex_one()
+    result = 0
+    for line in input:
+        muls = re.findall(pattern, line)
+        for mul in muls:
+            result += eval(mul)
+    return result
+
+def calc_part_two(input):
+    pattern_one = get_regex_one()
+    pattern_two = get_regex_two()
+    result = 0
+    for line in input:
+        muls = re.findall(pattern_one, line)
+        mul_one = muls[0]
+        result += eval(mul_one)
+    for line in input:
+        do_muls = re.findall(pattern_two, line)
+        print(do_muls)
+        for d in do_muls:
+            result += calc_part_one(do_muls)
+    return result
+
+def read_input():
+    with open("./day_three_input.txt", "r") as file:
+        return file.readlines()
+
+def get_regex_one():
+    return r'mul\(\d{1,3},\d{1,3}\)'
+
+def get_regex_two():
+    return r'do\(\).*?mul\(\d{1,3},\d{1,3}\)'
+
+def eval(str):
+    numbers = re.findall(r'\d+', str)
+    return reduce(mul,list(map(int, numbers)))
 
 
 if __name__ == '__main__':
     main()
+
--- tests/test_day_three.py
+++ tests/test_day_three.py
@@ -1,11 +1,22 @@
+import re
 import unittest
 
+import AdventOfCode.DayThree.day_three as three
 
 class TestDayThree(unittest.TestCase):
     def test_regex(self):
-        self.assertTrue(True)
+        self.assertTrue(re.fullmatch(three.get_regex_one(), "mul(1,3)"))
 
+    def test_regex_fail(self):
+        self.assertFalse(re.fullmatch(three.get_regex_one(), "mul(1,3"))
 
+    def test_teststring(self):
+        test_string = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        test_result = ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
+        self.assertEqual(re.findall(three.get_regex_one(), test_string), test_result)
 
+    def test_mul(self):
+        self.assertEqual(three.eval("mul(2,4)"), 8)
+    
 if __name__ == '__main__':
     unittest.main()
